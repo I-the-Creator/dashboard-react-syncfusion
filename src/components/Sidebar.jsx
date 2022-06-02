ec6ec523a1a8ccefc6deb8c831118df7c7c8c441
I-the-Creator <andrey.burluckiy@gmail.com>
2022-06-02 13:45:47 +0200

    Sidebar closing handler added; onClick functions updated

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,12 +4,22 @@ import { SiShopware } from "react-icons/si";
 import { MdOutlineCancel } from "react-icons/md";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
+// Context
+import { useStateContext } from "../context/ContextProvider";
+
 import { links } from "../data/dummy";
 
 const Sidebar = () => {
-  const activeMenu = true; // TEMP
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext();  // get state context from ContextProvider
 
-  const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-orange-600 text-lg m-2'
+  // to handle SideBar closing if 
+  const handleCloseSideBar = () => {
+    if(activeMenu && screenSize <= 900) {
+      setActiveMenu(false)
+    }
+  }
+
+  const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-orange-600 text-lg m-2' // styles when link is active
   const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-lg text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2'
 
   return (
@@ -19,16 +29,17 @@ const Sidebar = () => {
           <div className="flex justify-between items-center">
             <Link
               to="/"
-              onClick={() => {}}
+              onClick={handleCloseSideBar} // close sidebar after click on Logo if screen size < 900
               className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
             >
               <SiShopware />
               <span>Shoppy</span>
             </Link>
-            <TooltipComponent content="Menu" position="BottomCenter">
+            {/* Tooltip Component - the button to close SideBar */}
+            <TooltipComponent content="Menu" position="BottomCenter">  
               <button
                 type="button"
-                onClick={() => {}}
+                onClick={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)}
                 className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
               >
                 <MdOutlineCancel />
@@ -45,7 +56,7 @@ const Sidebar = () => {
                   <NavLink
                     to={`/${link.name}`}
                     key={link.name}
-                    onClick={() => {}}
+                    onClick={handleCloseSideBar}  // close sidebar after click if screen size < 900
                     className={({ isActive }) => isActive ? activeLink : normalLink }     // isActive - the NavLink inner state
                   >
                     {link.icon}
