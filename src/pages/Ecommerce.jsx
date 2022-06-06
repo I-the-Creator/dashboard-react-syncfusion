722f50c36e8e00c849cf3a19bef7eb0297fafe96
I-the-Creator <andrey.burluckiy@gmail.com>
2022-06-06 13:02:49 +0200

    fed with currentColor state - app context

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -11,6 +11,9 @@ import {
 import { useStateContext } from "../context/ContextProvider";
 
 const Ecommerce = () => {
+
+  const { currentColor } = useStateContext()   // theme Color from context
+
   return (
     <div className="mt-12">
       <div className="flex flex-wrap lg:flex-wrap justify-center">
@@ -24,7 +27,7 @@ const Ecommerce = () => {
           <div className="mt-6">
             <Button
               color="white"
-              bgColor="blue"
+              bgColor={currentColor}
               text="Download"
               borderRadius="10px"
               size="md"
@@ -88,19 +91,19 @@ const Ecommerce = () => {
 
               <div className="mt-5">
                 <SparkLine 
-                  currentColor="blue"
+                  currentColor={currentColor}
                   id="line-sparkline"
                   type="Line"
                   height="80px"
                   width="250px"
                   data={SparklineAreaData}
-                  color="blue"
+                  color={currentColor}
                 />
               </div>
               <div className="mt-10">
                 <Button
                   color="white"
-                  bgColor="blue"
+                  bgColor={currentColor}
                   text="Download Report"
                   borderRadius="10px"
                 />
