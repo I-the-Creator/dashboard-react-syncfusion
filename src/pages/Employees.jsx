551697c999f5a375830afc660882912cdbe3f8f7
I-the-Creator <andrey.burluckiy@gmail.com>
2022-06-13 19:02:14 +0200

    component fed with methods of syncfusion modules; layout updated

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Page,
+  Search,
+  Edit,
+  Inject,
+  Toolbar
+} from "@syncfusion/ej2-react-grids";
+
+import { employeesData, employeesGrid } from "../data/dummy";
+import { Header } from "../components";
 
 const Employees = () => {
   return (
-    <div>Employees</div>
-  )
-}
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Employees" />
+      <GridComponent
+        dataSource={employeesData}
+        allowPaging // pagination enabled
+        allowSorting // sorting enabled
+        toolbar={['Search']}  // search enabled
+        editSettings={{ allowDeleting: true, allowEditing: true }}
+        width="auto"
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject
+          services={[
+            Page,
+            Search,
+            Toolbar,
+            Edit
+          ]}
+        />
+      </GridComponent>
+    </div>
+  );
+};
 
-export default Employees
+export default Employees;
